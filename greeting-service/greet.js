0503210cee785f32ab834c92ae0d5436cc19243a
ThanawinW <thanawin.putter1@gmail.com>
2024-04-25 09:53:04 +0700

    Modify greeting message for v3

--- greeting-service/greet.js
+++ greeting-service/greet.js
@@ -3,6 +3,11 @@ module.exports = function greet(name) {
     return `Hello ${name || "guest"}!`;
 }
 */
+/*
 module.exports = function greet(name) {
     return `Hello ${name || "guest"} from version 2!`;
+}
+*/
+module.exports = function greet(name) {
+    return `Hello ${name || "guest"} from V3!`;
 }
